9f0bda9badf91baa8a7f5f8b1e96c2b811d01d59
samrickman <samrickman+github@gmail.com>
2023-09-27 20:06:20 +0100

    feat: auto restore renv if not present

--- 1__create_html_from_qmd.R
+++ 1__create_html_from_qmd.R
@@ -1,9 +1,12 @@
+# To manage R dependencies
+if (!require("renv")) install.packages("renv")
+
 # This function was to create the renv() the first time
 # it will never be run if the repo is cloned
 create_renv <- function() {
     if (dir.exists("renv")) {
         message("renv already created")
-        return(invisible(NULL))
+        return(invisible())
     }
     message("Creating renv...")
 
@@ -20,23 +23,38 @@ create_renv <- function() {
     quit()
 }
 
-create_directories <- function(dirs = c("./csv", "./img", "./raw_data")) {
+create_directory_structure <- function(dirs = c("./csv", "./img", "./raw_data")) {
     lapply(dirs, \(x) if (!dir.exists(x)) dir.create(x))
     invisible(NULL)
 }
 
-if (!require("renv")) install.packages("renv")
+restore_renv <- function() {
+    # If you git clone you don't get the actual libraries
+    # just the lockfile, so they need to be downloaded the first time
+    if (dir.exists("./renv/library/")) {
+        message("renv libraries already created.")
+        return(invisible())
+    }
+    message("renv libraries not found. Restoring...")
+    renv::restore()
+}
+
+create_html <- function() {
+    # Clean US election data (takes about 10 seconds to run)
+    message("Cleaning US election data...")
+
+    source("./2a__us_election_counties_clean_data.R")
+    message("Generating US cartogram...")
+    # Create Dorling cartogram (takes around 15 minutes to run!)
+    source("./2b__create_us_election_dorling_cartogram.R")
+
+    # Do the render from here - then no need to create a separate
+    # batch script for different OSes
+    system("quarto render ./GoLTC-maps-cartograms-blog-post.qmd")
+}
+
 
 create_renv()
-create_directories()
-
-
-# Clean US election data (takes about 10 seconds to run)
-message("Cleaning US election data...")
-source("./2a__us_election_counties_clean_data.R")
-message("Generating US cartogram...")
-# Create Dorling cartogram (takes around 15 minutes to run!)
-source("./2b__create_us_election_dorling_cartogram.R")
-
-# Then do the render
-system("quarto render ./GoLTC-maps-cartograms-blog-post.html")
+create_directory_structure()
+restore_renv()
+create_html()
